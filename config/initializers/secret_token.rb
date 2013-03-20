d7e8f14d8627b77f05ff93ad43879ff54fad7ec8
myun2 <myun2@nwhite.info>
2013-03-20 23:33:53 +0900

    config Add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aneken::Application.config.secret_token = 'df9d55bc3e55912bf410828b186f7dc9004a35f797086be10146e8027de632de7f67c07b14066e2243f3d6664394dffb80d9f790dc5813827681aebe54d718fc'
